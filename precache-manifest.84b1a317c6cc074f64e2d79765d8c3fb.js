c43428f2155a4a9f7602af5e78ee2c5ccf9c9a8f
William Lukas <iggyboy456@gmail.com>
2019-10-31 21:44:36 -0500

    Updates

---
+++ precache-manifest.84b1a317c6cc074f64e2d79765d8c3fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3904c4bf4caa605acbef95be02a089f",
+    "url": "/Click-game/index.html"
+  },
+  {
+    "revision": "ae964dc7e706e4158fca",
+    "url": "/Click-game/static/css/main.51a811b8.chunk.css"
+  },
+  {
+    "revision": "b42b009be4f2e6c686ce",
+    "url": "/Click-game/static/js/2.b151eaf4.chunk.js"
+  },
+  {
+    "revision": "ae964dc7e706e4158fca",
+    "url": "/Click-game/static/js/main.555b3883.chunk.js"
+  },
+  {
+    "revision": "c720bb762fb7fd9979c6",
+    "url": "/Click-game/static/js/runtime-main.bd0cd326.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Click-game/static/media/logo.25bf045c.svg"
+  }
+]);
